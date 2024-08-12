253d04e4727c06f35254d2f75cdf0895679419a8
William S. Moses <gh@wsmoses.com>
2024-08-11 21:29:39 -0400

    More c++ stdlib nofree

--- enzyme/Enzyme/EnzymeLogic.cpp
+++ enzyme/Enzyme/EnzymeLogic.cpp
@@ -6307,6 +6307,8 @@ llvm::Value *EnzymeLogic::CreateNoFree(RequestContext context,
       return GV;
     if (GV->getName() == "_ZSt4cout")
       return GV;
+    if (GV->getName() == "_ZNSt3__u5wcoutE")
+      return GV;
   }
 
   if (context.ip) {
