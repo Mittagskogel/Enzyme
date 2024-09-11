262048c8a10f6f5b53d18598382627a399221d3e
Faveo Hörold <faveo.hoerold@cscs.ch>
2024-09-11 17:42:25 +0200

    Add basic tests for Enzyme precision truncation in C and C++.

---
+++ prec-tests/basic-cpp/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir -p "${SCRIPT_DIR}/build"
+
+clang++ ./src/main.cpp $(pkg-config --libs --cflags mpfr gmp) -fno-exceptions -fpass-plugin=/scratch/fhrold/riken/Enzyme/enzyme/build/Enzyme/ClangEnzyme-20.so -Xclang -load -Xclang /scratch/fhrold/riken/Enzyme/enzyme/build/Enzyme/ClangEnzyme-20.so -Rpass=enzyme -mllvm -enzyme-truncate-all="64to2-2" -include enzyme/fprt/mpfr.h -o ./build/enzyme
+
+clang++ ./src/main.cpp -o ./build/normal
+
+echo "Normal:   " $(./build/normal 0.1 0.2)
+echo "Truncated:" $(./build/enzyme 0.1 0.2)
