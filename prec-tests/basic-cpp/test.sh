52375173173eee478d801ad4149c2ad51681ed13
Faveo Hörold <faveo.hoerold@cscs.ch>
2024-09-11 18:59:09 +0200

    Adjust relative paths in basic tests.

--- prec-tests/basic-cpp/test.sh
+++ prec-tests/basic-cpp/test.sh
@@ -4,9 +4,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 mkdir -p "${SCRIPT_DIR}/build"
 
-clang++ ./src/main.cpp $(pkg-config --libs --cflags mpfr gmp) -fno-exceptions -fpass-plugin=/scratch/fhrold/riken/Enzyme/enzyme/build/Enzyme/ClangEnzyme-20.so -Xclang -load -Xclang /scratch/fhrold/riken/Enzyme/enzyme/build/Enzyme/ClangEnzyme-20.so -Rpass=enzyme -mllvm -enzyme-truncate-all="64to2-2" -include enzyme/fprt/mpfr.h -o ./build/enzyme
+clang++ "${SCRIPT_DIR}/src/main.cpp" $(pkg-config --libs --cflags mpfr gmp) -fno-exceptions -fpass-plugin=/scratch/fhrold/riken/Enzyme/enzyme/build/Enzyme/ClangEnzyme-20.so -Xclang -load -Xclang /scratch/fhrold/riken/Enzyme/enzyme/build/Enzyme/ClangEnzyme-20.so -Rpass=enzyme -mllvm -enzyme-truncate-all="64to2-2" -include enzyme/fprt/mpfr.h -o "${SCRIPT_DIR}/build/enzyme"
 
-clang++ ./src/main.cpp -o ./build/normal
+clang++ "${SCRIPT_DIR}/src/main.cpp" -o "${SCRIPT_DIR}/build/normal"
 
-echo "Normal:   " $(./build/normal 0.1 0.2)
-echo "Truncated:" $(./build/enzyme 0.1 0.2)
+echo "Normal:   " $("${SCRIPT_DIR}/build/normal" 0.1 0.2)
+echo "Truncated:" $("${SCRIPT_DIR}/build/enzyme" 0.1 0.2)
+
