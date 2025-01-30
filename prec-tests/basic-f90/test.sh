f8e53c6aa34cbc0a1d0dc94db36388ad1f295f3d
Faveo Hörold <faveo.hoerold@cscs.ch>
2025-01-30 14:04:10 +0100

    Break lines in test.sh.

--- prec-tests/basic-f90/test.sh
+++ prec-tests/basic-f90/test.sh
@@ -5,9 +5,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p "${SCRIPT_DIR}/build"
 
 cp /scratch/fhrold/riken/Enzyme/enzyme/include/enzyme/fprt/mpfr.h "${SCRIPT_DIR}/src/mpfr.cpp"
-clang++ -c "${SCRIPT_DIR}/src/mpfr.cpp" $(pkg-config --cflags mpfr gmp) -I/scratch/fhrold/riken/Enzyme/enzyme/include/enzyme/fprt/ -o "${SCRIPT_DIR}/build/mpfr.o"
+clang++ -c "${SCRIPT_DIR}/src/mpfr.cpp" $(pkg-config --cflags mpfr gmp) \
+    -I/scratch/fhrold/riken/Enzyme/enzyme/include/enzyme/fprt/ -o "${SCRIPT_DIR}/build/mpfr.o"
 
-flang-new "${SCRIPT_DIR}/build/mpfr.o" "${SCRIPT_DIR}/src/main.f90" $(pkg-config --libs --cflags mpfr gmp) -fpass-plugin=/scratch/fhrold/riken/Enzyme/enzyme/build/Enzyme/LLVMEnzyme-20.so -Xflang -load -Xflang /scratch/fhrold/riken/Enzyme/enzyme/build/Enzyme/LLVMEnzyme-20.so -Rpass=enzyme -mllvm -enzyme-truncate-all="64to2-2" -o "${SCRIPT_DIR}/build/enzyme"
+flang-new "${SCRIPT_DIR}/build/mpfr.o" "${SCRIPT_DIR}/src/main.f90" \
+    $(pkg-config --libs --cflags mpfr gmp) \
+    -fpass-plugin=/scratch/fhrold/riken/Enzyme/enzyme/build/Enzyme/LLVMEnzyme-20.so \
+    -Xflang -load -Xflang /scratch/fhrold/riken/Enzyme/enzyme/build/Enzyme/LLVMEnzyme-20.so \
+    -Rpass=enzyme -mllvm -enzyme-truncate-all="64to2-2" -o "${SCRIPT_DIR}/build/enzyme"
 
 flang-new "${SCRIPT_DIR}/src/main.f90" -o "${SCRIPT_DIR}/build/normal"
 
