fcfc73c49e8fd4305bc0cfb4c641a9a9bd9a3222
Faveo Hörold <faveo.hoerold@cscs.ch>
2025-03-24 12:00:48 +0100

    Update test trunc-cpp.

--- prec-tests/trunc-cpp/test.sh
+++ prec-tests/trunc-cpp/test.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 mkdir -p "${SCRIPT_DIR}/build"
 
-cmake -DCMAKE_CXX_COMPILER=clang++ -B "${SCRIPT_DIR}/build" -S "${SCRIPT_DIR}/"
+cmake --fresh -DCMAKE_CXX_COMPILER=clang++ -B "${SCRIPT_DIR}/build" -S "${SCRIPT_DIR}/"
 cmake --build "${SCRIPT_DIR}/build"
 
 echo "Normal:   " $("${SCRIPT_DIR}/build/normal" 0.1 0.2)
